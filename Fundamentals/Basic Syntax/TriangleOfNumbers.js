55028676fcfa590cffd8f915798c39e6463c8898
Rozalia Ravnachka <rozalia.ravnachka2002@gmail.com>
2022-04-21 01:50:37 +0300

    Fundamentals

---
+++ Fundamentals/Basic Syntax/TriangleOfNumbers.js
@@ -0,0 +1,16 @@
+function TriangleOfNumbers(num){
+
+    for(let i = 1 ; i <= num; i++){
+        let line = "";
+        for(let j = 1; j <= i; j++){
+            line+=i;
+            if(j !== i){
+                line += " ";
+            }
+        }
+        console.log(line);
+    }
+    
+
+}
+TriangleOfNumbers(3)
