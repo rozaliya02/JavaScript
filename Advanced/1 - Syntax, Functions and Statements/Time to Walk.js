f07934423da59820b0f74352800375082220e355
Rozalia Ravnachka <rozalia.ravnachka2002@gmail.com>
2022-05-25 19:47:15 +0300

    Syntax,Functions and Statements

---
+++ Advanced/1 - Syntax, Functions and Statements/Time to Walk.js
@@ -0,0 +1,10 @@
+function solve(steps, footprint, speed) {}
+solve(4000, 0.6, 5);
+console.log("-------");
+solve(2564, 0.7, 5.5);
+
+// •	The first is the number of steps the student takes from their home to the university
+// •	Тhe second number is the length of the student's footprint in meters
+// •	Тhe third number is the student speed in km/h
+// Every 500 meters the student rests and takes a 1 minute break.
+// Calculate how long the student walks from home to university and print on the console the result in the following format: 'hours:minutes:seconds'.
