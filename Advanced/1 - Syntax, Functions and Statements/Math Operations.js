761fd13da9d2aaffee8804d641d398f1a5a84c90
Rozalia Ravnachka <rozalia.ravnachka2002@gmail.com>
2022-05-25 19:46:22 +0300

    Syntax,Functions and Statements

---
+++ Advanced/1 - Syntax, Functions and Statements/Math Operations.js
@@ -0,0 +1,16 @@
+function solve(num1, num2, operator) {
+  if (operator === "+") {
+    console.log(num1 + num2);
+  } else if (operator === "-") {
+    console.log(num1 - num2);
+  } else if (operator === "*") {
+    console.log(num1 * num2);
+  } else if (operator === "/") {
+    console.log(num1 / num2);
+  } else if (operator === "%") {
+    console.log(num1 % num2);
+  } else if (operator === "**") {
+    console.log(num1 ** num2);
+  }
+}
+solve(5, 6, "+");
